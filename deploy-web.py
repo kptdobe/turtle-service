d9c7ce8924613bbfa66e983d903c99d29404058a
Alexandre Capt <acapt@adobe.com>
2023-11-20 11:23:25 +0100

    fix: loading

--- deploy-web.py
+++ deploy-web.py
@@ -1,32 +1,34 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS,cross_origin
 from fastai.vision.all import *
+import __main__
+
+def is_cat(x): return x[0].isupper()
+__main__.is_cat = is_cat
+
 app = Flask(__name__)
 CORS(app, support_credentials=True)
 
-if __name__ == '__main__':
-    def is_cat(x): return x[0].isupper()
+# load the learner
+learn = load_learner('./model/model.pkl')
+categories = ('Dog', 'Cat')
 
-    # load the learner
-    learn = load_learner('./model/model.pkl')
-    categories = ('Dog', 'Cat')
+def classify_images(img):
+    i = PILImage.create(img)
+    pred, idx, probs = learn.predict(i)
 
-    def classify_images(img):
-        i = PILImage.create(img)
-        #'Is it a car?', 'Is it a car? but as zero or one', 'probabillity of [dog, cat]'
-        pred, idx, probs = learn.predict(i)
-        #return dictionary
-        #zip together the categories and the
-        #turn probs to float
-    
-        return { 
-            'prediction': categories[idx],
-            'probabilities': dict(zip(categories, map(float, probs)))
-        }
+    return { 
+        'prediction': categories[idx],
+        'probabilities': dict(zip(categories, map(float, probs)))
+    }
 
-    # route for prediction
-    @app.route('/predict', methods=['POST'])
-    def predict():
-        return jsonify(classify_images(request.files['image']))
+# route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    return jsonify(classify_images(request.files['image']))
 
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello, do you want to know where is your turtle?'
+
+app.run()
